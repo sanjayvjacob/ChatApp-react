4fb052d7d811644889313c19faec9319189c8130
sanjayvjacob <sanjayvinodjacob@gmail.com>
2024-07-10 21:30:39 +0100

    added routes

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,32 @@
-//create express server
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5001;
 
-app.get("/", (req, res) => {
-  //root route http://localhost:5001/
-  res.send("Hello World!");
-});
+dotenv.config();
+
+app.use(express.json()); // Middleware to parse incoming JSON payloads (from req.body)
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Connect to MongoDB
+connectToMongoDB()
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    // Start the server after successfully connecting to MongoDB
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB:", err.message);
+  });
